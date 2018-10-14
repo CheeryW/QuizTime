45525dd057478b621ddb69824fd1d5d66e83cea7
CheeryW <cheeryone821@gmail.com>
2018-10-13 17:00:36 -0700

    Optional features added

--- app/src/main/java/com/example/cheery/quiztime/MainActivity.java
+++ app/src/main/java/com/example/cheery/quiztime/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cheery.quiztime;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,11 +12,83 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // flip to show answer
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
+
+        // flip to show question
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // Click answer1 (wrong)
+        findViewById(R.id.answer1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer1).setBackgroundColor(getColor(R.color.richRed));
+                findViewById(R.id.answer3).setBackgroundColor(getColor(R.color.algaeGreen));
+            }
+        });
+
+        // Click answer2 (wrong)
+        findViewById(R.id.answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer2).setBackgroundColor(getColor(R.color.richRed));
+                findViewById(R.id.answer3).setBackgroundColor(getColor(R.color.algaeGreen));
+            }
+        });
+
+        // Click answer3 (right)
+        findViewById(R.id.answer3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer3).setBackgroundColor(getColor(R.color.algaeGreen));
+            }
+        });
+
+        // Reset
+        findViewById(R.id.layout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer1).setBackgroundColor(getColor(R.color.grey));
+                findViewById(R.id.answer2).setBackgroundColor(getColor(R.color.grey));
+                findViewById(R.id.answer3).setBackgroundColor(getColor(R.color.grey));
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // Show answer choices
+        findViewById(R.id.visible).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer1).setVisibility(View.VISIBLE);
+                findViewById(R.id.answer2).setVisibility(View.VISIBLE);
+                findViewById(R.id.answer3).setVisibility(View.VISIBLE);
+                findViewById(R.id.invisible).setVisibility(View.VISIBLE);
+                findViewById(R.id.visible).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // Hide answer choices
+        findViewById(R.id.invisible).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
+                findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
+                findViewById(R.id.answer3).setVisibility(View.INVISIBLE);
+                findViewById(R.id.visible).setVisibility(View.VISIBLE);
+                findViewById(R.id.invisible).setVisibility(View.INVISIBLE);
             }
         });
     }
