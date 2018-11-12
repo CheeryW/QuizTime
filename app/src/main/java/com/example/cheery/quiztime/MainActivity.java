a0b61ca9bd2359b3ef173cb06e03154d9f159631
CheeryW <cheeryone821@gmail.com>
2018-11-12 02:13:04 -0800

    More features added for lab in week 6 (lab 3)

--- app/src/main/java/com/example/cheery/quiztime/MainActivity.java
+++ app/src/main/java/com/example/cheery/quiztime/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected static boolean editClicked = false;
     FlashcardDatabase flashcardDatabase;
     List<Flashcard> allFlashcards; // hold all flashcard objects
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,72 @@ public class MainActivity extends AppCompatActivity {
             ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
             ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
         }
+
+        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(allFlashcards.size() != 0) {
+                    currentCardDisplayedIndex++; // show the next card by adding index
+
+                    if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                        currentCardDisplayedIndex = 0; // when we reach the end of card list
+                    }
+
+                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+
+                    // always keep the question side of the next card up
+                    if (findViewById(R.id.flashcard_answer).getVisibility() == View.VISIBLE) {
+                        findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                        findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+
+        findViewById(R.id.prev_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(allFlashcards.size() != 0) {
+                    currentCardDisplayedIndex--; // show the previous card by adding index
+
+                    if (currentCardDisplayedIndex < 0) {
+                        currentCardDisplayedIndex = allFlashcards.size() - 1; // when we reach the end of card list
+                    }
+
+                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+
+                    // always keep the question side of the previous card up
+                    if (findViewById(R.id.flashcard_answer).getVisibility() == View.VISIBLE) {
+                        findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                        findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+
+        // delete a card
+        findViewById(R.id.delete_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardDatabase.deleteCard(((TextView) findViewById(R.id.flashcard_question)).getText().toString());
+                allFlashcards = flashcardDatabase.getAllCards(); // update new list of cards
+
+                // prompt user to add a card if no card is available
+                if(allFlashcards.size() == 0) {
+                    ((TextView) findViewById(R.id.flashcard_question)).setText("Add a Question!");
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText("Add an Answer!");
+                    currentCardDisplayedIndex = 0;
+                } else {
+                    currentCardDisplayedIndex--;
+                    if (currentCardDisplayedIndex < 0) {
+                        currentCardDisplayedIndex = allFlashcards.size() - 1; // when we reach the end of card list
+                    }
+                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                }
+            }
+        });
 
         // flip to show answer
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
